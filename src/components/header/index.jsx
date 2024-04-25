ec61afb504d62ee9f87be04f7ba50ada95e6f3ec
unechevre <enzonezo1@gmail.com>
2024-04-25 18:32:15 +0200

    ajout header home

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Importez useNavigate
 import HeaderLogo from "../../ressources/pictures/Kouwik2.png";
 import './header.css';
 
 function Header({ votesLeft }) {
+  const navigate = useNavigate(); // Utilisez le hook useNavigate pour la navigation
+
+  // Fonction pour gérer le clic sur le logo
+  const goToHome = () => {
+    navigate('/'); // Navigue vers la page d'accueil
+  };
+
   return (
     <header className="header">
-      <img src={HeaderLogo} alt="Logo" className="logo" />
+      <img src={HeaderLogo} alt="Logo" className="logo" onClick={goToHome} /> // Ajoutez l'écouteur d'événement onClick
       <div className="vote-counter">
         Votes Left: {votesLeft}
       </div>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import CreateBoardButton from '../../components/createBoard';
-
+import Header from '../../components/header';
 function HelloWorld() {
   return (
   <>
-
+    <Header votesLeft={0}/>
     <CreateBoardButton/>
 
   </>
